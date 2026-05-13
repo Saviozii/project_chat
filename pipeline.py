de1502874ecf311dabe50757cc653168e988fcd8
Saviozii <ramonsantosfernandes2016@gmail.com>
2026-05-13 19:57:50 -0300

    Initial commit: chat project setup

---
+++ pipeline.py
@@ -0,0 +1,97 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from qdrant_client import QdrantClient, models
+from qdrant_client.models import Distance, VectorParams
+
+
+Ebook_pdf = "data/Ebook.pdf"
+
+model_embedding = "bge-m3"
+Ollama_Url = "http://localhost:11434"
+model_dimencoes = 1024
+
+Qdrant_Url = "http://localhost:6333"
+COLLETION = "Guia_Ebook"
+
+CHUNK_SIZE = 600
+CHUNK_OVERLAP = 100
+
+
+#Pego o arquivo
+def ler_pdf():
+    loader = PyPDFLoader(Ebook_pdf)
+    documentos = loader.load()
+    return documentos
+
+documentos = ler_pdf()
+
+#Divido o arquivo
+def dividir_documento(documentos):
+    chunk = RecursiveCharacterTextSplitter(
+        chunk_size = CHUNK_SIZE,
+        chunk_overlap  = CHUNK_OVERLAP,
+        )
+    chunks = chunk.split_documents(documentos)
+    return chunks
+
+chunks = dividir_documento(documentos)
+
+#Embedding do arquivo
+model = OllamaEmbeddings(
+    model = model_embedding,
+    base_url = Ollama_Url,
+)
+def gerar_embedding(chunks):
+    data = [chunk.page_content for chunk in chunks]
+    embeddings = model.embed_documents(data)
+    return embeddings
+
+embeddings = gerar_embedding(chunks)
+
+#Adcionar ao banco vetorial.
+
+Qdrant_client = QdrantClient(url=Qdrant_Url)
+
+def criar_banco_qdrant():
+    colecoes = [i.name for i in Qdrant_client.get_collections().collections]
+
+
+    if COLLETION in colecoes:
+        print("Ja tem")
+    else:
+        Qdrant_client.create_collection(
+            collection_name= COLLETION,
+            vectors_config=models.VectorParams(size= model_dimencoes,
+            distance=models.Distance.COSINE),
+            )
+
+
+criar_banco_qdrant()
+
+def adcionar_points(chunks, embeddings):
+    pontos = []
+
+    for i in range(len(chunks)):
+        ponto = models.PointStruct(
+            id= i,
+            vector= embeddings[i],
+            payload={
+                "texto": chunks[i].page_content,
+                "pagina": chunks[i].metadata.get("page", None),
+                "fonte": chunks[i].metadata.get("source", None),
+            },
+        )
+        pontos.append(ponto)
+    return pontos
+
+pontos = adcionar_points(chunks,embeddings)
+
+def enviar_qdrant(points):
+    Qdrant_client.upsert(
+        collection_name= COLLETION,
+        points= points
+    )
+    print("Enviado")
+
+enviar_qdrant(pontos)
